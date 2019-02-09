205f33ec05cc6b6e67286a18d267697d8435b809
Andrii Osipov <a.osipov2>
2019-02-09 16:08:30 +0100

    Updates

---
+++ precache-manifest.a20a4a0c8f29db6dc0365b7eaa0e8638.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d6e05c7c8a94f522f5e040d7f079b2f",
+    "url": "/Apartment-site-portfolio/static/media/5.1 Sofa-Balcony.6d6e05c7.jpg"
+  },
+  {
+    "revision": "decedd49df984340663a",
+    "url": "/Apartment-site-portfolio/static/css/main.2b738613.chunk.css"
+  },
+  {
+    "revision": "9a32ed9d3f617cd2c0db7e80c9ae4538",
+    "url": "/Apartment-site-portfolio/static/media/microwave.9a32ed9d.svg"
+  },
+  {
+    "revision": "0f89c8e759c685ab6fb7",
+    "url": "/Apartment-site-portfolio/static/js/1.0f89c8e7.chunk.js"
+  },
+  {
+    "revision": "69cd4d4f87cea37ba5dc",
+    "url": "/Apartment-site-portfolio/static/js/runtime~main.69cd4d4f.js"
+  },
+  {
+    "revision": "6a44c448db354e54cff86dc161096dcb",
+    "url": "/Apartment-site-portfolio/static/media/Logo.6a44c448.png"
+  },
+  {
+    "revision": "d65ed2fdf786a68361a1ef85454a752e",
+    "url": "/Apartment-site-portfolio/static/media/1. Building.d65ed2fd.jpg"
+  },
+  {
+    "revision": "bd03629623c9efc2703f64a04809381f",
+    "url": "/Apartment-site-portfolio/static/media/1.1 Entrance.bd036296.jpg"
+  },
+  {
+    "revision": "66683ded5071b2613999d77d6256989c",
+    "url": "/Apartment-site-portfolio/static/media/2. Hall.66683ded.jpg"
+  },
+  {
+    "revision": "7251a1820bb76f393560a770032c661b",
+    "url": "/Apartment-site-portfolio/static/media/3 Bathroom.7251a182.jpg"
+  },
+  {
+    "revision": "810159cf1e8cd7715fee1c89c85a3bad",
+    "url": "/Apartment-site-portfolio/static/media/3.1 Bath.810159cf.jpg"
+  },
+  {
+    "revision": "b0965a0b49bad99fe7fd22c8d7a2e434",
+    "url": "/Apartment-site-portfolio/static/media/3.2 Bath.b0965a0b.jpg"
+  },
+  {
+    "revision": "90f5252f7a333a04ec09da5c1f437a43",
+    "url": "/Apartment-site-portfolio/static/media/3.4 BATH.90f5252f.jpg"
+  },
+  {
+    "revision": "ec819de4e6061d2ebfa5f6e1fbbd3d92",
+    "url": "/Apartment-site-portfolio/static/media/4. Bar.ec819de4.jpg"
+  },
+  {
+    "revision": "e871a27b0bdd9f3674a24f84134137da",
+    "url": "/Apartment-site-portfolio/static/media/5. Sofa.e871a27b.jpg"
+  },
+  {
+    "revision": "decedd49df984340663a",
+    "url": "/Apartment-site-portfolio/static/js/main.decedd49.chunk.js"
+  },
+  {
+    "revision": "f1f93192c492291f880d558ee45b44ad",
+    "url": "/Apartment-site-portfolio/static/media/5.2 Sofa.f1f93192.jpg"
+  },
+  {
+    "revision": "7afe0c515fb66397ba4f1e0e486b13e1",
+    "url": "/Apartment-site-portfolio/static/media/6. Kitchen.7afe0c51.jpg"
+  },
+  {
+    "revision": "ed37d63c6be92abb68bf48cc13ac3754",
+    "url": "/Apartment-site-portfolio/static/media/6.1 Kitchen.ed37d63c.jpg"
+  },
+  {
+    "revision": "4fae0f379da5aaee66fb3ccbadc8867f",
+    "url": "/Apartment-site-portfolio/static/media/7. Bed.4fae0f37.jpg"
+  },
+  {
+    "revision": "15d3d4bef09e66a8970d1e46eba590ed",
+    "url": "/Apartment-site-portfolio/static/media/7.1 Bed-Balcony.15d3d4be.jpg"
+  },
+  {
+    "revision": "b675d36c84977e4b960dacc6759930b4",
+    "url": "/Apartment-site-portfolio/static/media/8. Full view.b675d36c.jpg"
+  },
+  {
+    "revision": "352d4c2c7d892f8649fd15617b72ee02",
+    "url": "/Apartment-site-portfolio/static/media/accenture.352d4c2c.png"
+  },
+  {
+    "revision": "a842ee8012a18f7dca58570d47bfc0f8",
+    "url": "/Apartment-site-portfolio/static/media/google.a842ee80.png"
+  },
+  {
+    "revision": "5480bbb5900adff0bf82758a43d7b935",
+    "url": "/Apartment-site-portfolio/static/media/_free-icons_png_512_1298722.5480bbb5.png"
+  },
+  {
+    "revision": "d36d749310ea7f153c7e15905fc642ab",
+    "url": "/Apartment-site-portfolio/static/media/iron.d36d7493.svg"
+  },
+  {
+    "revision": "2b46a6f23409ab50d32bc8b30646e72a",
+    "url": "/Apartment-site-portfolio/static/media/shampoo.2b46a6f2.svg"
+  },
+  {
+    "revision": "1a026170b42506412d162c14b5d00b09",
+    "url": "/Apartment-site-portfolio/static/media/towels.1a026170.svg"
+  },
+  {
+    "revision": "0f89c8e759c685ab6fb7",
+    "url": "/Apartment-site-portfolio/static/css/1.a537d2ea.chunk.css"
+  },
+  {
+    "revision": "a8bcee9ed04f71c6a3f85bb88265d2c0",
+    "url": "/Apartment-site-portfolio/index.html"
+  }
+];
